e91b6f1abfad099e029e7a62aae0694f422fbf0f
Hugo Nunes <hugoanunes2003@gmail.com>
2026-01-15 15:21:24 -0300

    Novo estilo de organizacao de pastas das aulas adicionado e aula sobre os metodos remove e replace de array adicionado

---
+++ arrays/metodos/remove_Replace/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace remove_Replace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Remove()
+
+            string data = "12345John Smith         5000 3   ";
+
+            string updateData = data.Remove(5, 20); // Remove(index, quantidade);
+
+            System.Console.WriteLine(updateData);
+
+            System.Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-");
+
+            // Replace()
+
+            string message = "This--is--ex-amp-le--da-ta";
+            message = message.Replace("--", " "); // Replace("substituir o que?", "para o que?");
+            message = message.Replace("-", "");
+            System.Console.WriteLine(message);
+
+        }
+    }
+}
