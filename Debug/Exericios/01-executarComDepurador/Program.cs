fe7423c4d29303ee78044bc6df6ebd4606f39969
Hugo Nunes <hugoanunes2003@gmail.com>
2026-01-26 17:22:56 -0300

    Finalizando o curso adicionando novas aulas, exercicios e projetos

---
+++ Debug/Exericios/01-executarComDepurador/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DebugTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  
+            This code instantiates a value and then calls the ChangeValue method
+            to update the value. The code then prints the updated value to the console.
+            */
+            int x = 5;
+
+            ChangeValue(ref x);
+
+            Console.WriteLine(x);
+
+            void ChangeValue(ref int value) 
+            {
+                value = 10;
+            }
+        }
+    }
+}
---
+++ Debug/Exericios/02-tratamentoDeExecoes/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ExceptionTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process1();
+            }
+            catch
+            {
+                System.Console.WriteLine("An exception has been caught.");
+            }
+
+            Console.WriteLine("Exit program");
+
+
+        static void Process1()
+        {
+                try
+                {
+                  WriteMessage();  
+                }
+                catch(DivideByZeroException ex) // Aqui ele está preparado para tratar apenas a exeção DIVIDEBYZERO, nada mais.
+                {
+                    System.Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+                }
+            
+        }
+
+        static void WriteMessage()
+        {
+            double float1 = 3000.0;
+            double float2 = 0.0;
+            int number1 = 3000;
+            int number2 = 0;
+            byte smallNumber;
+
+            Console.WriteLine(float1 / float2);
+            Console.WriteLine(number1 / number2);
+                // checked
+                // {
+                //     smallNumber = (byte) number1;
+                // }
+        }
+
+        }
+    }
+}
